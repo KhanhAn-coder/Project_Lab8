afb8b22e253fe25c2630d2eb1e8adfaaadec3471
Nguyen Van Khanh An <51900475@student.tdtu.edu.vn>
2023-04-22 16:13:34 +0700

    Update Lab8

--- app/src/main/java/com/example/a51900475_lab8/MainActivity.java
+++ app/src/main/java/com/example/a51900475_lab8/MainActivity.java
@@ -67,5 +67,50 @@ public class MainActivity extends AppCompatActivity {
         /**
          GET List Users
          **/
+        Call<UserList> call2 = apiInterface.doGetUserList("2");
+        call2.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
+
+                for (UserList.Datum datum : datumList) {
+                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+
+            }
+        });
+
+        Call<UserList> call3 = apiInterface.doCreateUserWithField("morpheus","leader");
+        call3.enqueue(new Callback<UserList>() {
+            @Override
+            public void onResponse(Call<UserList> call, Response<UserList> response) {
+                UserList userList = response.body();
+                Integer text = userList.page;
+                Integer total = userList.total;
+                Integer totalPages = userList.totalPages;
+                List<UserList.Datum> datumList = userList.data;
+                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
+
+                for (UserList.Datum datum : datumList) {
+                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserList> call, Throwable t) {
+                call.cancel();
+            }
+        });
     }
 }
